b1865189d013c565d8525253d044e3a3df539472
shelsoren <shelsoren@gmail.com>
2023-10-10 10:33:50 -0400

    updates to SignIn page - shelby

--- pawns-and-puzzles/src/Components/Sign in Page/SignIn.js
+++ pawns-and-puzzles/src/Components/Sign in Page/SignIn.js
@@ -13,18 +13,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
@@ -119,7 +108,7 @@ export default function SignInSide() {
                   </Link>
                 </Grid>
               </Grid>
-              <Copyright sx={{ mt: 5 }} />
+              
             </Box>
           </Box>
         </Grid>
