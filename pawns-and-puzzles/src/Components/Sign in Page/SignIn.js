8b227b802a3c1e6bb3bc89a7ed805bfe4c9f2df0
shelsoren <shelsoren@gmail.com>
2023-10-12 09:26:09 -0400

    updates to theme of sign in page part two

--- pawns-and-puzzles/src/Components/Sign in Page/SignIn.js
+++ pawns-and-puzzles/src/Components/Sign in Page/SignIn.js
@@ -1,21 +1,16 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import checkerboard from "../../Assets/background/CheckerboardBackground.jpg"
 
 
-<<<<<<< HEAD
 
 const customTheme = createTheme({
   typography: {
@@ -23,17 +18,14 @@ const customTheme = createTheme({
   },
   palette: {
     primary: {
-      main: '#0f4a3b', // Set the primary color to your desired color
+      main: '#0f4a3b', 
     },
     text: {
-      primary: '#000', // Black color for text
-      secondary: '#fff', // White color for secondary text
+      primary: '#000', 
+      secondary: '#fff', 
     },
   },
 });
-=======
-const defaultTheme = createTheme();
->>>>>>> b6cc04f2083def5307d96a8d4ef6c607e8e9f8d2
 
 export default function SignInSide() {
     const handleSubmit = (event) => {
@@ -45,7 +37,6 @@ export default function SignInSide() {
         });
     };
 
-<<<<<<< HEAD
   return (
     <ThemeProvider theme={customTheme}>
       <Grid container component="main" sx={{ height: '100vh' }}>
@@ -117,93 +108,3 @@ export default function SignInSide() {
     </ThemeProvider>
   );
 }
-=======
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Grid container component="main" sx={{ height: '100vh' }}>
-                <CssBaseline />
-                <Grid
-                    item
-                    xs={false}
-                    sm={4}
-                    md={7}
-                    sx={{
-                        backgroundImage: 'url(pawns-and-puzzles/src/assets/CheckerboardBackground.jpg)',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundColor: (t) =>
-                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    }}
-                />
-                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                    <Box
-                        sx={{
-                            my: 8,
-                            mx: 4,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                        </Typography>
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
-                            </Grid>
-
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </ThemeProvider>
-    );
-
-}
->>>>>>> b6cc04f2083def5307d96a8d4ef6c607e8e9f8d2
